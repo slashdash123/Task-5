6f6e6b3f16e520ed9443a070b65173c7044a014d
Colna <domen.colnaric@gmail.com>
2022-06-15 20:44:54 +0200

    first commit

---
+++ Inc/printf.h
@@ -0,0 +1,110 @@
+/*
+ * printf.h
+ *
+ *  Created on: Oct 20, 2020
+ *      Author: ales.cep
+ *      Original file: core_cm4.h
+ */
+
+#ifndef PRINTF_H_
+#define PRINTF_H_
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct
+{
+  volatile  union
+  {
+    volatile  uint8_t    u8;                 /*!< Offset: 0x000 ( /W)  ITM Stimulus Port 8-bit */
+    volatile  uint16_t   u16;                /*!< Offset: 0x000 ( /W)  ITM Stimulus Port 16-bit */
+    volatile  uint32_t   u32;                /*!< Offset: 0x000 ( /W)  ITM Stimulus Port 32-bit */
+  }  PORT [32U];                         /*!< Offset: 0x000 ( /W)  ITM Stimulus Port Registers */
+        uint32_t RESERVED0[864U];
+  volatile uint32_t TER;                    /*!< Offset: 0xE00 (R/W)  ITM Trace Enable Register */
+        uint32_t RESERVED1[15U];
+  volatile uint32_t TPR;                    /*!< Offset: 0xE40 (R/W)  ITM Trace Privilege Register */
+        uint32_t RESERVED2[15U];
+  volatile uint32_t TCR;                    /*!< Offset: 0xE80 (R/W)  ITM Trace Control Register */
+        uint32_t RESERVED3[29U];
+  volatile  uint32_t IWR;                    /*!< Offset: 0xEF8 ( /W)  ITM Integration Write Register */
+  volatile const  uint32_t IRR;                    /*!< Offset: 0xEFC (R/ )  ITM Integration Read Register */
+  volatile uint32_t IMCR;                   /*!< Offset: 0xF00 (R/W)  ITM Integration Mode Control Register */
+        uint32_t RESERVED4[43U];
+  volatile  uint32_t LAR;                    /*!< Offset: 0xFB0 ( /W)  ITM Lock Access Register */
+  volatile const  uint32_t LSR;                    /*!< Offset: 0xFB4 (R/ )  ITM Lock Status Register */
+       uint32_t RESERVED5[6U];
+  volatile const  uint32_t PID4;                   /*!< Offset: 0xFD0 (R/ )  ITM Peripheral Identification Register #4 */
+  volatile const  uint32_t PID5;                   /*!< Offset: 0xFD4 (R/ )  ITM Peripheral Identification Register #5 */
+  volatile const  uint32_t PID6;                   /*!< Offset: 0xFD8 (R/ )  ITM Peripheral Identification Register #6 */
+  volatile const  uint32_t PID7;                   /*!< Offset: 0xFDC (R/ )  ITM Peripheral Identification Register #7 */
+  volatile const  uint32_t PID0;                   /*!< Offset: 0xFE0 (R/ )  ITM Peripheral Identification Register #0 */
+  volatile const  uint32_t PID1;                   /*!< Offset: 0xFE4 (R/ )  ITM Peripheral Identification Register #1 */
+  volatile const  uint32_t PID2;                   /*!< Offset: 0xFE8 (R/ )  ITM Peripheral Identification Register #2 */
+  volatile const  uint32_t PID3;                   /*!< Offset: 0xFEC (R/ )  ITM Peripheral Identification Register #3 */
+  volatile const  uint32_t CID0;                   /*!< Offset: 0xFF0 (R/ )  ITM Component  Identification Register #0 */
+  volatile const  uint32_t CID1;                   /*!< Offset: 0xFF4 (R/ )  ITM Component  Identification Register #1 */
+  volatile const  uint32_t CID2;                   /*!< Offset: 0xFF8 (R/ )  ITM Component  Identification Register #2 */
+  volatile const  uint32_t CID3;                   /*!< Offset: 0xFFC (R/ )  ITM Component  Identification Register #3 */
+} ITM_Type;
+
+static inline uint32_t ITM_SendChar (uint32_t ch)
+{
+  #define ITM ((ITM_Type*)0xE0000000UL)   /*!< ITM configuration struct */
+  if (((ITM->TCR & 1UL) != 0UL) &&      /* ITM enabled */
+      ((ITM->TER & 1UL               ) != 0UL)   )     /* ITM Port #0 enabled */
+  {
+    while (ITM->PORT[0U].u32 == 0UL)
+    {
+    	__asm volatile ("nop");
+    }
+    ITM->PORT[0U].u8 = (uint8_t)ch;
+  }
+  return (ch);
+}
+
+int _write(int file, char *ptr, int len)
+{
+  /* Implement your write code here, this is used by puts and printf for example */
+  int i=0;
+  for(i=0 ; i<len ; i++)
+    ITM_SendChar((*ptr++));
+  return len;
+}
+
+static inline void print7SegmentDisplay(){
+	uint32_t* fOdr = (uint32_t*)(0x40021414);
+	printf("+-------+\n");
+	printf("| %s |\n", (*fOdr & (1 << 0)) == 0 ? " --- " : "     ");
+	printf("| %c   %c |\n", (*fOdr & (1 << 5)) == 0 ? '|' : ' ', (*fOdr & (1 << 1)) == 0 ? '|' : ' ');
+	printf("| %s |\n", (*fOdr & (1 << 6)) == 0 ? " --- " : "     ");
+	printf("| %c   %c |\n", (*fOdr & (1 << 4)) == 0 ? '|' : ' ', (*fOdr & (1 << 2)) == 0 ? '|' : ' ');
+	printf("| %s.|\n", (*fOdr & (1 << 3)) == 0 ? " --- " : "     ");
+	printf("+-------+\n");
+
+
+	//Delay ~2 seconds
+	for(int i = 0; i < 1000; i++)
+		for(int j = 0; j < 2800; j++){}
+}
+static inline void printLEDBar(){
+	uint32_t* fOdr = (uint32_t*)(0x40021414);
+
+	printf("%c%c%c%c%c%c%c%c%c%c\n",
+				(*fOdr & (1 << 0)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 1)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 2)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 3)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 4)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 5)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 6)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 7)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 8)) != 0 ? '-' : ' ',
+				(*fOdr & (1 << 9)) != 0 ? '-' : ' ');
+
+
+	//Delay
+	for(int i = 0; i < 100; i++)
+		for(int j = 0; j < 2800; j++){}
+}
+
+#endif /* PRINTF_H_ */
