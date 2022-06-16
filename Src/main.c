2d19d2c9d786d671827007f33f8d3290b3b447ac
Colna <60469511+slashdash123@users.noreply.github.com>
2022-06-16 10:18:23 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -31,27 +31,27 @@ int main(void)
 {
 	/********************************** CLOCK CONFIG **********************************/
 
-	SET_BIT(RCC -> AHB1ENR, RCC_AHB1ENR_GPIOEEN); 					// vklop ure GPIOE
-	SET_BIT(RCC -> APB1ENR, RCC_APB1ENR_TIM4EN); 					// vklop ure TIM4 general purpose
-	SET_BIT(RCC -> APB1ENR, RCC_APB1ENR_TIM7EN); 					// vklop ure TIM7 basic
+	SET_BIT(RCC -> AHB1ENR, RCC_AHB1ENR_GPIOEEN); 				// vklop ure GPIOE
+	SET_BIT(RCC -> APB1ENR, RCC_APB1ENR_TIM4EN); 				// vklop ure TIM4 general purpose
+	SET_BIT(RCC -> APB1ENR, RCC_APB1ENR_TIM7EN); 				// vklop ure TIM7 basic
 
 	/********************************** TIMER CONFIG **********************************/
 
 	/*************	Konfiguracija TIM4 - general purpose timer za 5 sekundno zakasnitev	*************/
-	TIM4->CR1 &= ~TIM_CR1_DIR;										// DIR = 0 => Counter used as upcounter
-	TIM4->PSC = 0;													// fCNT = fCLK/(PSC+1)/(ARR+1) = 16MHz/1/16000 = 1kHz => perioda T = 1/f = 1 ms
-	TIM4->ARR = 16000-1;											// ARR doseze v 1 ms => overflow vsako 1 ms
-	//TIM4->CR1 |= TIM_CR1_URS;										// URS = 1 => generate interrupt when overflow/underflow
-	TIM4->DIER |= TIM_DIER_UIE;										// UIE = 1 => update interrupt enable
-	TIM4->EGR |= TIM_EGR_UG;										// UG = 1 =>  re-initialize the counter and generate an update of registers; ročno sprozi dogodek
-	TIM4->SR &= ~TIM_SR_UIF;										// Pocistimo UIF bit
-	//TIM4->CR1 |= TIM_CR1_CEN;										// Enable timer -> naredimo v delay funkciji
-	NVIC_EnableIRQ(TIM4_IRQn);										// Omogoci prekinitve
+	TIM4->CR1 &= ~TIM_CR1_DIR;						// DIR = 0 => Counter used as upcounter
+	TIM4->PSC = 0;								// fCNT = fCLK/(PSC+1)/(ARR+1) = 16MHz/1/16000 = 1kHz => perioda T = 1/f = 1 ms
+	TIM4->ARR = 16000-1;							// ARR doseze v 1 ms => overflow vsako 1 ms
+	//TIM4->CR1 |= TIM_CR1_URS;						// URS = 1 => generate interrupt when overflow/underflow
+	TIM4->DIER |= TIM_DIER_UIE;						// UIE = 1 => update interrupt enable
+	TIM4->EGR |= TIM_EGR_UG;						// UG = 1 =>  re-initialize the counter and generate an update of registers; ročno sprozi dogodek
+	TIM4->SR &= ~TIM_SR_UIF;						// Pocistimo UIF bit
+	//TIM4->CR1 |= TIM_CR1_CEN;						// Enable timer -> naredimo v delay funkciji
+	NVIC_EnableIRQ(TIM4_IRQn);						// Omogoci prekinitve
 
 	/************* Konfiguracija TIM7 - basic timer za namen branja iz senzorja - counter used as upcounter *************/
-	TIM7->PSC = 0;													// fCNT = fCLK/(PSC+1)/(ARR+1) = 16MHz/1/16000 = 1kHz => perioda T = 1/f = 1 ms
-	TIM7->ARR = 16000-1;											// Ta ARR doseze v 1 ms, do te ne bo prestel med branjem (beremo pa v podrocju us)
-	//TIM7->CR1 |= TIM_CR1_CEN;										// Enable timer -> naredimo med branjem
+	TIM7->PSC = 0;								// fCNT = fCLK/(PSC+1)/(ARR+1) = 16MHz/1/16000 = 1kHz => perioda T = 1/f = 1 ms
+	TIM7->ARR = 16000-1;							// Ta ARR doseze v 1 ms, do te ne bo prestel med branjem (beremo pa v podrocju us)
+	//TIM7->CR1 |= TIM_CR1_CEN;						// Enable timer -> naredimo med branjem
 
 	/********************************** PIN CONFIG **********************************/
 
@@ -65,7 +65,7 @@ int main(void)
 
 	for(;;)
 	{
-		Delay_ms(5000);	 											// 5 sekundni delay (zagon TIM4)
+		Delay_ms(5000);	 						// 5 sekundni delay (zagon TIM4)
 		//printf("Test delay\n");
 
 		/********************************** Branje iz senzorja AM2302 **********************************/
@@ -86,37 +86,37 @@ int main(void)
 		CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODER5_1);
 		CLEAR_BIT(GPIOE->MODER, GPIO_MODER_MODER5_0);
 
-		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) != 0);			// čakaj na TLOW
-		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) == 0);			// čakaj na THIGH (Treh)
-		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) != 0);			// čakaj na TLOW
+		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) != 0);		// čakaj na TLOW
+		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) == 0);		// čakaj na THIGH (Treh)
+		while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) != 0);		// čakaj na TLOW
 
-		for(uint8_t i = 0; i < 40; i++)								// beremo 40 bitov
+		for(uint8_t i = 0; i < 40; i++)					// beremo 40 bitov
 		{
 				while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) == 0 );		// Preveri IDR in cakaj dokler IDR = 0 -> cakaj na THigh
 				// s TIM7 beri cas HIGH, beri CNT, izracunaj cas THigh, doloci ali gre za 0 ali za 1, resetiraj counter CNT na 0
-				TIM7->CNT = 0;											// Resetiraj counter na 0
-				TIM7->CR1 |= TIM_CR1_CEN;								// Enable timer
+				TIM7->CNT = 0;							// Resetiraj counter na 0
+				TIM7->CR1 |= TIM_CR1_CEN;					// Enable timer
 
 				while( READ_BIT(GPIOE->IDR,GPIO_IDR_IDR_5) != 0 );		// cakaj na TLOW
-				TIM7->CR1 &= ~TIM_CR1_CEN;								// Disable timer
+				TIM7->CR1 &= ~TIM_CR1_CEN;					// Disable timer
 
 				// Preverimo ali je poslan bit = 0  ali = 1, shrani vrednost v RawData spremenljivko
 				if(READ_REG(TIM7->CNT) < 800)			// Poslan je bil bit 0 (Thigh < 50 us => TH0; CNT=800 => 50us [CNT = 15 999 => 1 ms])
 				{
-					rawData <<= 1;						// Dodaj bit 0 skrajno desno
+					rawData <<= 1;				// Dodaj bit 0 skrajno desno
 				}
-				else									// poslan je bil bit 1 (THigh > 50 us => TH1)
+				else						// poslan je bil bit 1 (THigh > 50 us => TH1)
 				{
 					rawData = (rawData << 1) | 1;		// Dodaj bit 1 skrajno desno
 				}
 		}
 
 		// Iz 64bit podatka s shiftanjem ipd. izvleci spremenljivki Vlaga16_t in Temp16_t
-		uint8_t ParityBit = (uint8_t)rawData;							// skrajno desnih 8 bitov
-		uint8_t TempLow = (uint8_t)(rawData>>8);						// naslednjih 8 bitov
-		uint8_t TempHigh = (uint8_t)(rawData>>16);						// naslednjih 8 bitov
-		uint8_t HumidityLow = (uint8_t)(rawData>>24);					// naslednjih 8 bitov
-		uint8_t HumidityHigh = (uint8_t)(rawData>>32);					// zadnjih 8 bitov
+		uint8_t ParityBit = (uint8_t)rawData;				// skrajno desnih 8 bitov
+		uint8_t TempLow = (uint8_t)(rawData>>8);			// naslednjih 8 bitov
+		uint8_t TempHigh = (uint8_t)(rawData>>16);			// naslednjih 8 bitov
+		uint8_t HumidityLow = (uint8_t)(rawData>>24);			// naslednjih 8 bitov
+		uint8_t HumidityHigh = (uint8_t)(rawData>>32);			// zadnjih 8 bitov
 
 		printf("ParityBit = %d\n",ParityBit);
 		printf("Vsota = %d\n",(uint8_t)(HumidityHigh+HumidityLow+TempHigh+TempLow));
@@ -131,7 +131,7 @@ int main(void)
 				printf("Vlaga = %d.%1d %%RH\n",Vlaga16_t/10, Vlaga16_t%10);
 
 				// Izpis temperature
-				uint8_t sign = Temp16_t >> 15;											// MSb doloca predznak
+				uint8_t sign = Temp16_t >> 15;						// MSb doloca predznak
 
 				if(sign == 0)
 				{
@@ -139,7 +139,7 @@ int main(void)
 				}
 				else
 				{
-					Temp16_t &= ~(1<<15);												// Zbrisi bit, ki doloca predznak
+					Temp16_t &= ~(1<<15);						// Zbrisi bit, ki doloca predznak
 					printf("Temperatura = -%d.%1d C\n",Temp16_t/10, Temp16_t%10);
 				}
 		}																				// konec if parity bit validation
@@ -153,11 +153,11 @@ int main(void)
 
 void Delay_ms(int16_t ms)
 {
-	TIM4->CNT = 0;													// Resetiraj counter na 0
+	TIM4->CNT = 0;					// Resetiraj counter na 0
 	overflowCounter = 0;
-	TIM4->CR1 |= TIM_CR1_CEN;										// Enable timer
-	while(overflowCounter < ms);									// Cakaj dokler se ne zgudi ms stevilo prekinitev (vsak overflow pomeni 1 ms)
-	TIM4->CR1 &= ~TIM_CR1_CEN;										// Disable timer
+	TIM4->CR1 |= TIM_CR1_CEN;			// Enable timer
+	while(overflowCounter < ms);			// Cakaj dokler se ne zgudi ms stevilo prekinitev (vsak overflow pomeni 1 ms)
+	TIM4->CR1 &= ~TIM_CR1_CEN;			// Disable timer
 }
 
 
@@ -165,8 +165,8 @@ void TIM4_IRQHandler(void)
 {
 	// Prekinitvena funkcija za delay
 	if( READ_BIT(TIM4->SR,TIM_SR_UIF) == 1 )
-	{																// razlog za prekinitev: timer je prestel do ARR (counter overflow)
+	{						// razlog za prekinitev: timer je prestel do ARR (counter overflow)
 		overflowCounter++;
-		CLEAR_BIT(TIM4->SR,TIM_SR_UIF);								// Pocistimo UIF bit (drugace procesor ves cas klice to prekinitveno rutino)
+		CLEAR_BIT(TIM4->SR,TIM_SR_UIF);		// Pocistimo UIF bit (drugace procesor ves cas klice to prekinitveno rutino)
 	}
 }
